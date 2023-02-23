aa1079c62a98764c1b994823a51c81486e83da5f
SirAgha <6zgbdyf@code.edu.az>
2023-02-24 02:55:05 +0400

    Boomin

--- Medicoz/Areas/manage/Controllers/DepartmentController.cs
+++ Medicoz/Areas/manage/Controllers/DepartmentController.cs
@@ -2,6 +2,7 @@
 using Medicoz.Helpers;
 using Medicoz.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Security;
 
 namespace Medicoz.Areas.manage.Controllers
 {
@@ -9,10 +10,12 @@ namespace Medicoz.Areas.manage.Controllers
     public class DepartmentController : Controller
     {
         private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
 
-        public DepartmentController(AppDbContext context)
+        public DepartmentController(AppDbContext context,IWebHostEnvironment env)
         {
             _context = context;
+            _env = env;
         }
         public IActionResult Index(int page=1)
         {
@@ -28,6 +31,22 @@ namespace Medicoz.Areas.manage.Controllers
         public IActionResult Create(Department department)
         {
             if(!ModelState.IsValid) return View();
+            if(department.ImageFile is null)
+            {
+                ModelState.AddModelError("ImageFile", "Can't be null");
+                return View();
+            }
+            if (department.ImageFile.Length > 2097152)
+            {
+                ModelState.AddModelError("ImageFile", "Image size must be 2mb or lower");
+                return View();
+            }
+            if (department.ImageFile.ContentType != "image/png" && department.ImageFile.ContentType != "image/jpeg")
+            {
+                ModelState.AddModelError("ImageFile", "Image type must be png, jpg or jpeg");
+                return View();
+            }
+            department.Imageurl = department.ImageFile.SaveFile("uploads/departments", _env.WebRootPath);
             _context.Departments.Add(department); 
             _context.SaveChanges();
             return RedirectToAction("index");
@@ -44,7 +63,31 @@ namespace Medicoz.Areas.manage.Controllers
             Department exstdepartment=_context.Departments.FirstOrDefault(x=>x.Id == department.Id);
             if(exstdepartment==null) return View("Error");
             if (!ModelState.IsValid) return View();
+            if(department.ImageFile != null)
+            {
+                if (department.ImageFile.Length > 2097152)
+                {
+                    ModelState.AddModelError("ImageFile", "Image size must be 2mb or lower");
+                    return View();
+                }
+                if (department.ImageFile.ContentType != "image/png" && department.ImageFile.ContentType != "image/jpeg")
+                {
+                    ModelState.AddModelError("ImageFile", "Image type must be png, jpg or jpeg");
+                    return View();
+                }
+                string path = Path.Combine(_env.WebRootPath, "uploads/departments", exstdepartment.Imageurl);
+                if (System.IO.File.Exists(path))
+                {
+                    System.IO.File.Delete(path);
+                }
+                exstdepartment.Imageurl = department.ImageFile.SaveFile("uploads/departments", _env.WebRootPath);
+            }
             exstdepartment.Name = department.Name;
+            exstdepartment.Tittle = department.Tittle;
+            exstdepartment.Description1 = department.Description1;
+            exstdepartment.Description2 = department.Description2;
+            exstdepartment.IconUrl=department.IconUrl;
+            
             _context.SaveChanges();
             return RedirectToAction("index");
 
@@ -53,6 +96,11 @@ namespace Medicoz.Areas.manage.Controllers
         {
             Department department = _context.Departments.FirstOrDefault(x => x.Id == id);
             if (department == null) return View("Error");
+            string path = Path.Combine(_env.WebRootPath, "uploads/departments", department.Imageurl);
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
             _context.Departments.Remove(department);
             _context.SaveChanges();
             return RedirectToAction("index");
--- Medicoz/Models/Department.cs
+++ Medicoz/Models/Department.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Medicoz.Models
 {
@@ -7,7 +8,22 @@ namespace Medicoz.Models
         public int Id { get; set; }
         [StringLength(maximumLength:30)]
         public string Name { get; set; }
+        [StringLength(maximumLength:60)]
+        public string Tittle { get; set; }
+
+        [StringLength(maximumLength:400)]
+        public string Description1 { get; set; }
+        [StringLength(maximumLength:300)]
+        public string? Description2 { get; set; }
+
+        [StringLength(maximumLength: 100)]
+
+        public string? Imageurl { get; set; }
+        [StringLength(maximumLength:100)]
+        public string IconUrl { get; set; }
         public List<Doctor>? Doctors { get; set; }
+        [NotMapped]
+        public IFormFile? ImageFile { get; set; }
 
     }
 }
